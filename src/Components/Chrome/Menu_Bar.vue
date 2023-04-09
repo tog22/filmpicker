f58dd41da94168850288e8e791306e8986b9879d
tog22 <tom.ash.github@philosofiles.com>
2023-04-09 16:56:20 -0700

    *HOW* Add dropdown menu

--- src/Components/Chrome/Menu_Bar.vue
+++ src/Components/Chrome/Menu_Bar.vue
@@ -9,9 +9,36 @@
 			</h1>
 		</div>
 		<div class="s_right">
-			<span class="menu_icon" @click="toggle_menu">
-				☰
-			</span>
+			<div class="menu_container">
+				<div 
+					class="menu_icon" 
+					@click="is_menu_open = !is_menu_open"
+				>
+					☰
+
+					<!-- @mouseover="is_menu_open = true"
+					@mouseleave="is_menu_open = false" -->
+				</div>
+				<div 
+					id="menu_position"
+				>
+					<div 
+						v-if="is_menu_open"
+						id="basic_menu" 
+					>
+						<p
+							v-for="link in menu_links"
+							:key="link.title"
+							v-bind="link"
+						>
+							<a :href="link.url">
+								<span v-html="link.title">
+								</span>
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 	<Debug_Display/>
@@ -41,9 +68,23 @@ export default {
 	components: { 
 		// Debug_Display 
 	},
-	methods: {
-		toggle_menu() {
-			
+	data() {
+		return {
+			is_menu_open: 		false,
+			menu_links:			[
+				{
+					title:		'Home',
+					url:		'/'
+				},
+				{
+					title:		'Log in',
+					url:		'/login'
+				},
+				{
+					title:		'Sign up',
+					url:		'/signup'
+				}
+			]
 		}
 	}
 }
@@ -51,5 +92,45 @@ export default {
 
 
 <style scoped>
+.menu_icon {
+	cursor: pointer;
+}
+
+#menu_position {
+	position: relative;
+}
+
+#basic_menu {
+	position: absolute;
+	top: -1px;
+	right: 0;
+
+	background-color: white;
+	border: 1px solid black;
+
+	font-size: 0.8em;
+	white-space: nowrap;
+	padding: 1em 2em 1.2em;
+}
+
+#basic_menu p:first-child {
+	margin-top: 0;
+}
+
+#basic_menu p:last-child {
+	margin-bottom: 0;
+}
+
+#basic_menu p,
+#basic_menu a,
+#basic_menu a:visited,
+#basic_menu a:active {
+	color: rgb(0,0,238);
+	cursor: pointer;
+}
+
+#basic_menu a:hover {
+	color: black;
+}
 
 </style>  
